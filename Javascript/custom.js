723af300e44a2e014b493af6d837729ad12e0d4f
beherasurya <beherasuryakanta18@gmail.com>
2022-12-16 19:50:40 +0530

    changing contents of DOM

--- Javascript/custom.js
+++ Javascript/custom.js
@@ -1,15 +1,21 @@
-console.log('Hello World');
+// console.log('Hello World');
 
-var images=document.getElementsByTagName('img');
-console.log(images);
+// var images=document.getElementsByTagName('img');
+// console.log(images);
 
-var anchors=document.getElementsByTagName('a');
-console.log(anchors);
+// var anchors=document.getElementsByTagName('a');
+// console.log(anchors);
 
-var paragraphs=document.getElementsByTagName('p');
-console.log(paragraphs);
+// var paragraphs=document.getElementsByTagName('p');
+// console.log(paragraphs);
+
+// var heading=document.getElementById('heading');
+// console.log(heading);
+// console.log(heading.innerHTML);
+// console.log(heading.nodeType);
+
+var main_content=document.getElementById('main-content');
+main_content.setAttribute('align','right');
 
 var heading=document.getElementById('heading');
-console.log(heading);
-console.log(heading.innerHTML);
-console.log(heading.nodeType);
+heading.innerHTML="Welcome to GWSST";
