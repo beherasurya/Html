9321959f2e417e4d0a89668d6b82828314560605
beherasurya <beherasuryakanta18@gmail.com>
2022-12-16 17:18:30 +0530

    Working with DOM elements

---
+++ Javascript/custom.js
@@ -0,0 +1,15 @@
+console.log('Hello World');
+
+var images=document.getElementsByTagName('img');
+console.log(images);
+
+var anchors=document.getElementsByTagName('a');
+console.log(anchors);
+
+var paragraphs=document.getElementsByTagName('p');
+console.log(paragraphs);
+
+var heading=document.getElementById('heading');
+console.log(heading);
+console.log(heading.innerHTML);
+console.log(heading.nodeType);
