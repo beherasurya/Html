f1502e4258c2dc097f4dbb9b0d6519e7ed991e57
beherasurya <beherasuryakanta18@gmail.com>
2022-12-16 20:04:04 +0530

    Adding events to DOM elements

--- Javascript/custom.js
+++ Javascript/custom.js
@@ -20,13 +20,20 @@
 // var heading=document.getElementById('heading');
 // heading.innerHTML="Welcome to GWSST";
 
-var main_content=document.getElementById('main-content');
+// var main_content=document.getElementById('main-content');
 
-var new_heading=document.createElement('h2');
-var new_phrase=document.createElement('p');
+// var new_heading=document.createElement('h2');
+// var new_phrase=document.createElement('p');
 
-new_heading.innerHTML='Welcome to GWSST';
-new_phrase.innerHTML='This is a simple phrase just to show a demo';
+// new_heading.innerHTML='Welcome to GWSST';
+// new_phrase.innerHTML='This is a simple phrase just to show a demo';
 
-main_content.appendChild(new_heading);
-main_content.appendChild(new_phrase);
+// main_content.appendChild(new_heading);
+// main_content.appendChild(new_phrase);
+
+var btn=document.getElementById('myBtn');
+btn.onclick=function()
+{
+    alert('Hello World!');
+    alert('Bye World');
+};
